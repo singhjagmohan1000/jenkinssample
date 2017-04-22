462fb1d51b8147bce7978c38c3b68733bf756336
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-04-22 03:11:53 -0700

    chaning test cases

--- test/test.js
+++ test/test.js
@@ -6,6 +6,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/').expect('hello world', done);
+        request(app).get('/').expect('Hello, How are You Jenkins Jagmohan', done);
     });
 });
