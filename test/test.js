07b338a37f4397deca344b4b4d690c4e30c506fb
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-04-22 02:05:23 -0700

    test script is created

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gaggi on 4/22/17.
+ */
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+    it('respond with hello world', function(done) {
+        request(app).get('/').expect('hello world', done);
+    });
+});
